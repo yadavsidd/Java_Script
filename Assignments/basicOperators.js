9ffcd9568859a5f964c1ff1763f25a3bbfb4201e
Sidd <sidddhant.10@gmail.com>
2024-06-06 09:57:01 +0530

    commit6/6

---
+++ Assignments/basicOperators.js
@@ -0,0 +1,16 @@
+let language = 'Hindi';
+let country = 'india';
+let continent = 'Asia';
+let indiaPopulation = 100;
+let finlandPopulation = 600;
+let avgPopulation = 33;
+indiaPopulation= indiaPopulation * 0.5;
+const description1 =
+  country +
+  ' is in ' +
+  continent +
+  ', and its ' +
+  indiaPopulation +
+  ' million people speak ' +
+  language;
+console.log(description1);
---
+++ Assignments/const.js
@@ -0,0 +1,24 @@
+/*
+let, const and var﻿
+Set the value of language to the language spoken where you live (some countries have multiple languages, but just choose one).
+
+Think about which variables should be const variables (which values will never change, and which might change?). Then, change these variables to const.
+
+Try to change one of the changed variables now, and observe what happens.
+*/
+
+language = 'Hindi';
+const country ='India';
+const continent ='Asia';
+const isIsland =true;
+//isIsland = false;
+//the above line causes error
+console.log(language);
+console.log(country);
+console.log(continent);
+console.log(isIsland);
+
+
+
+
+
